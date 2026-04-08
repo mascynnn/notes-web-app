4064fe9f177c1ad604dbf24232342b40629a624a
mascynnn <makhasinmuhammad355@gmail.com>
2026-04-08 14:17:50 +0700

    improve UI design

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Notes App</title>
+    <link rel="stylesheet" href="assets/css/style.css">
+</head>
+<body>
+
+<div class="container">
+
+<h2>📝 Notes Web App</h2>
+
+<p style="text-align:center; color:#555;">
+    Simpan ide kamu dengan mudah dan cepat 🚀
+</p>
+
+<p style="text-align:center;">
+    <a href="notes.php">
+        <button>Masuk ke Notes</button>
+    </a>
+</p>
+
+</div>
+
+</body>
+</html>
